eaee13f4a05d58c65c9548729538ded7370d7d47
sadness_ojisan <sadness.ojisan@gmail.com>
2018-10-07 19:38:52 +0900

    fix: export pkg

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import Center from './components/center/Center'
 import Font from './components/font/Font'
 
-export default {Center, Font}
+export {Center, Font}
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [{
