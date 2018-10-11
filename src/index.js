6fde73ba24379368165d4ed656b799e8ceee19e5
sadness_ojisan <sadness.ojisan@gmail.com>
2018-10-12 01:10:55 +0900

    update: pkg version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Center from './components/center/Center'
 import Font from './components/font/Font'
 import Blink from './components/blink/Blink'
+import Marquee from './components/marquee/Marquee'
 
-export {Center, Font, Blink}
+export {Center, Font, Blink, Marquee}
