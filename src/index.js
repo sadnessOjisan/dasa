9a6aed5e87ae588b3bd5536f8deaaa09789b3ec0
sadness_ojisan <sadness.ojisan@gmail.com>
2018-10-13 13:16:14 +0900

    update: export gradation & outline

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import Blink from './components/blink/Blink'
 import Marquee from './components/marquee/Marquee'
 import Nobr from './components/nobr/Nobr'
 import Tile from './components/tile/Tile'
+import OutlineText from './components/outlineText/OutlineText'
+import GradationText from './components/gradationText/GradationText'
 
-export {Center, Font, Blink, Marquee, Nobr, Tile}
+export {Center, Font, Blink, Marquee, Nobr, Tile, OutlineText, GradationText}
