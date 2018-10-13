076d6cc2af6c2fbbf9f7a87d62617bc2423ef70b
sadness_ojisan <sadness.ojisan@gmail.com>
2018-10-13 18:27:47 +0900

    update: add samples in center tag

--- src/components/center/Center.stories.js
+++ src/components/center/Center.stories.js
@@ -3,12 +3,20 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import Center from './Center';
+import Tile from '../tile/Tile';
+import GIMG from '../tile/example_image.png'
 
 storiesOf('Center', module)
-  .add('with vertical horizontal', () => (
-    <Center vertical={true} horizontal={true}>
-      <span>
-        😀 😎 👍 💯
-      </span>
-    </Center>
-  ));
+  .add('center', () => (
+    <Tile image={GIMG} isGlobal={true}> <Center vertical={true} horizontal={true}>
+    😀 😎 👍 💯
+</Center></Tile>
+  )).add('center vertical', () => (
+    <Tile image={GIMG} isGlobal={true}> <Center vertical={true}>
+    😀 😎 👍 💯
+</Center></Tile>
+     )).add('center horizontal', () => (
+      <Tile image={GIMG} isGlobal={true}> <Center horizontal={true}>
+      😀 😎 👍 💯
+  </Center></Tile>
+  ))
