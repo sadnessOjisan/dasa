2c1465cbef41fb438e67b3a6c8e4d16d21d6efdc
sadness_ojisan <sadness.ojisan@gmail.com>
2018-10-13 11:47:47 +0900

    update: export tile

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import Font from './components/font/Font'
 import Blink from './components/blink/Blink'
 import Marquee from './components/marquee/Marquee'
 import Nobr from './components/nobr/Nobr'
+import Tile from './components/tile/Tile'
 
-export {Center, Font, Blink, Marquee, Nobr}
+export {Center, Font, Blink, Marquee, Nobr, Tile}
