396a3a4ba6801e67e22e9d6a017c5948404d768b
sadness_ojisan <sadness.ojisan@gmail.com>
2018-10-13 18:32:13 +0900

    remove: unused line

--- src/components/center/Center.stories.js
+++ src/components/center/Center.stories.js
@@ -9,7 +9,6 @@ import GIMG from "../tile/example_image.png";
 storiesOf("Center", module)
   .add("center", () => (
     <Tile image={GIMG} isGlobal={true}>
-      {" "}
       <Center vertical={true} horizontal={true}>
         😀 😎 👍 💯
       </Center>
@@ -17,13 +16,11 @@ storiesOf("Center", module)
   ))
   .add("center vertical", () => (
     <Tile image={GIMG} isGlobal={true}>
-      {" "}
       <Center vertical={true}>😀 😎 👍 💯</Center>
     </Tile>
   ))
   .add("center horizontal", () => (
     <Tile image={GIMG} isGlobal={true}>
-      {" "}
       <Center horizontal={true}>😀 😎 👍 💯</Center>
     </Tile>
   ));
